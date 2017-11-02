3b8b8467ac6fe27de481480ea5e718c63c485c01
Splimis <splimis@gmail.com>
2017-11-02 11:28:52 -0400

    additional page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navpills from "./components/Navpills";
 import Home from "./components/pages/Home";
 import Create from "./components/pages/Create"
+import Travel from "./components/pages/Travel"
 
 
 
@@ -19,6 +20,7 @@ class App extends Component {
             <Navpills />
             <Route exact path="/" component={Home} />
             <Route exact path="/create" component={Create} />
+            <Route exact path="/travel" component={Travel} />
           </div>
 
         </Router>
---
+++ client/src/components/pages/Travel.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Travel = () =>
+  <div>
+    <h1>There should be forms here</h1>
+    <p>
+      <form>
+        <h1>RENDER ME!!!</h1>
+        <input type = "text" name = "start"/>
+        <br/>
+        <input type = "text" name = "begin"/>
+
+
+      </form>
+    </p>
+  </div>;
+
+export default Travel;
