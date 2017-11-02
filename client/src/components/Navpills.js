9a365a1eeb4bb774145da34210fa5a08642064f5
Splimis <splimis@gmail.com>
2017-11-02 11:09:47 -0400

    minor changes

--- client/src/components/Navpills.js
+++ client/src/components/Navpills.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Navpills = () =>
   <ul className="nav nav-tabs">
     <li className={window.location.pathname === "/" ? "active" : ""}>
-      <Link to="/">Home</Link>
+      <Link to="/">Log In</Link>
     </li>
     <li className={window.location.pathname === "/create" ? "active" : ""}>
-      <Link to="/create">Create</Link>
+      <Link to="/create">Create an Account</Link>
     </li>
   </ul>;
 
