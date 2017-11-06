cc6fc775ddbb11bb127d471566fdb9b7922faf29
Ryan Franco <rfranco92@gmail.com>
2017-11-06 13:01:22 -0500

    Fixing everything

--- client/src/components/pages/login.js
+++ client/src/components/pages/login.js
@@ -1,78 +1,46 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
-import LoginForm from '../login/loginForm';
-import SignupForm from '../login/SignUpForm';
-import Header from '../login/header';
-import Home from "../login/home";
+import React, { Component } from "react";
+import LoginForm from "../login/loginForm.js"
+import axios from 'axios'
 
-class login extends Component {
+
+class Login extends Component  {
+
 	constructor() {
 		super()
 		this.state = {
-			loggedIn: false,
-			user: null
 		}
-		this._logout = this._logout.bind(this)
-		this._login = this._login.bind(this)
+		this._login = this._login.bind(this);
 	}
-	componentDidMount() {
-		axios.get('/auth/user').then(response => {
-			console.log(response.data)
-			if (!!response.data.user) {
-				console.log('THERE IS A USER')
-				this.setState({
-					loggedIn: true,
-					user: response.data.user
-				})
-			} else {
-				this.setState({
-					loggedIn: false,
-					user: null
-				})
-			}
-		})
+	_login(username, password) {
+		axios
+			.post('/auth/login', {
+				username,
+				password
+			})
+			.then(response => {
+				console.log(response)
+				if (response.status === 200) {
+					// update the state
+					this.setState({
+						loggedIn: true,
+						user: response.data.user
+					})
+					window.location.reload();
+				}
+			})
 	}
-
-	_logout(event) {
-		event.preventDefault()
-		console.log('logging out')
-		axios.post('/auth/logout').then(response => {
-			console.log(response.data)
-			if (response.status === 200) {
-				this.setState({
-					loggedIn: false,
-					user: null
-				})
-			}
-		})
+	render() {
+		return( 
+  <div>
+    <h1>Please log in using your username and password</h1>
+    <p>     
+        <LoginForm 
+        _login={this._login}
+		_googleSignin={this._googleSignin}
+		/>
+    </p>
+  </div>
+  )
 	}
-}
-
-render(){ 
-		return (
-			<div className="App">
-				<h1>This is the main App component</h1>
-				<Header user={this.state.user} />
-				{/* LINKS to our different 'pages' */}
-				<Home _logout={this._logout} loggedIn={this.state.loggedIn} />
-				{/*  ROUTES */}
-				{/* <Route exact path="/" component={Home} /> */}
-				<Route exact path="/" render={() => <Home user={this.state.user} />} />
-				<Route
-					exact
-					path="/login"
-					render={() =>
-						<LoginForm
-							_login={this._login}
-							_googleSignin={this._googleSignin}
-						/>}
-				/>
-				<Route exact path="/signup" component={SignupForm} />
-				{/* <LoginForm _login={this._login} /> */}
-			</div>
-		)
-	}
-
-
-export default login;
+}	
+export default Login;
