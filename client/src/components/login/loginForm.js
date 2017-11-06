49b477f7e6830ec3060fb156761027f6ab7b9643
Ryan Franco <rfranco92@gmail.com>
2017-11-06 10:58:38 -0500

    changed state

--- client/src/components/login/loginForm.js
+++ client/src/components/login/loginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import axios from 'axios'
+import Header from "./header"
 // import googleButton from './google_signin_buttons/web/1x/btn_google_signin_dark_disabled_web.png'
 import googleButton from './google_signin_buttons/web/1x/btn_google_signin_dark_normal_web.png'
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,7 +4,7 @@ import LoginForm from "../login/loginForm.js"
 const Home = () =>
   <div>
     <h1>Please log</h1>
-    <p>        Render log in here
+    <p>     
         <LoginForm 
         _login={this._login}
 		_googleSignin={this._googleSignin}
