b035e10f49f5f200d8d6256832c4299c18547bfa
Splimis <splimis@gmail.com>
2017-11-09 11:48:48 -0500

    mo style

--- client/src/components/Form/Input.js
+++ client/src/components/Form/Input.js
@@ -3,9 +3,8 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import DropDown from "../DropDown";
+import axios from "axios"
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
-
-
 export class Input extends React.Component {
     constructor(props) {
     super(props)
@@ -22,10 +21,8 @@ export class Input extends React.Component {
     this.handleChange = this.handleChange.bind(this)
     this.handleChanges = this.handleChanges.bind(this)
   }
-
   handleFormSubmit(event) {
-  	event.preventDefault()
-
+    event.preventDefault()
     geocodeByAddress(this.state.address1)
       .then((results) => getLatLng(results[0]))
       .then(({ lat, lng }) => {
@@ -38,7 +35,6 @@ export class Input extends React.Component {
       .catch((error) => {
         console.log('Oh no!', error)
       })
-
      geocodeByAddress(this.state.address2)
       .then((results) => getLatLng(results[0]))
       .then(({ lat, lng }) => {
@@ -51,33 +47,52 @@ export class Input extends React.Component {
       .catch((error) => {
         console.log('Oh no!', error)
       })
-
-  }
-
+      axios
+      .post('/api/createtrip', {
+        address: this.state.geocodeResults1,
+        end: this.state.geocodeResults2,
+        startDate: this.state.start,
+        endDate: this.state.end
+      })
+      .then(response => {
+        console.log(response)
+        if (!response.data.error) {
+          this.setState({
+            redirectTo: '/currenttrips'
+          })
+        } else {
+          alert(response.data.error)
+        }
+      })
+     }  
+    handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+    };
   handleChange(address1) {
     this.setState({
       address1,
-      geocodeResults: null
+      geocodeResults1: null
     })
   }
-
   handleChanges(address2) {
     this.setState({
       address2,
-      geocodeResults: null
+      geocodeResults2: null
     })
   }
   
-
   render() {
-
     const AutocompleteItem = ({ formattedSuggestion }) => (
       <div className="Demo__suggestion-item">
         <i className='fa fa-map-marker Demo__suggestion-icon'/>
         <strong>{formattedSuggestion.mainText}</strong>{' '}
         <small className="text-muted">{formattedSuggestion.secondaryText}</small>
       </div>)
-
     const inputStartProps = {
       type: "text",
       value: this.state.address1,
@@ -87,7 +102,6 @@ export class Input extends React.Component {
       autoFocus: true,
       placeholder: "Starting Address"
     }
-
     const inputEndProps = {
       type: "text",
       value: this.state.address2,
@@ -97,35 +111,46 @@ export class Input extends React.Component {
       autoFocus: true,
       placeholder: "Ending Address"
     }
-
     return (
-    	<div classname="container">
-          <div className="Row">
+        <div className="col-lg-10">
 
-      <div className="col-lg-3">
-      </div>
-    
-        <div className="col-lg-3">
-  	      <form onSubmit={this.handleFormSubmit}>
+            <div className="col-lg-3">
+            </div>
+
+          
+          <form onSubmit={this.handleFormSubmit}>
+            <div className="col-lg-3">
+
         <PlacesAutocomplete 
             autocompleteItem={AutocompleteItem}
             inputProps={inputStartProps} />
-            </form>
-          </div>
+          <input
+            className="farts"
+            value={this.state.start}
+            name="start"
+            onChange={this.handleInputChange}
+            type="date"
+            placeholder="Starting Date"
+            />
+          </div>  
 
-         <div className="col-lg-3"> 
-          <form onSubmit={this.handleFormSubmit}>
+          <div className="col-lg-3">  
         <PlacesAutocomplete 
             autocompleteItem={AutocompleteItem}
             inputProps={inputEndProps} />
-          </form>
-        </div>
-  <button>Submit</button>
-        </div> 
-  
-      </div> // container
+          <input
+            className="farts"
+            value={this.state.end}
+            name="end"
+            onChange={this.handleInputChange}
+            type="date"
+            placeholder="Ending Date"
+            />
+          <button className="pissShit">Submit</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
-
 export default Input;
--- client/src/components/pages/Travel.js
+++ client/src/components/pages/Travel.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Link, Route } from "react-router-dom";
 import { Input, TextArea, FormBtn, EndPut } from "../../components/Form";
 import FooterBar from "../../components/FooterBar";
+import "./Travel.css"
 
 
 
 const Travel = props =>
   <div>
-    <h1>Plan Your Location</h1>
+    <h1 className="assHole">Plan Your Location</h1>
     <Input>
     </Input>
 
