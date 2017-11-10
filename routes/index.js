ba459305963f40e9e34590793d19bf5966d36f77
Ryan Franco <rfranco92@gmail.com>
2017-11-10 11:43:13 -0500

    path

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const router = require("express").Router();
 const authRoutes = require("./api/login");
 const apiRoutes = require("./api/trips")
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const morgan = require('morgan');
+const path = require("path");
 const session = require('express-session');
 const passport = require('./passport')
 const MongoStore = require('connect-mongo')(session)
