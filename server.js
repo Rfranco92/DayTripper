5a1e4bac4e188fbfec0e90a5129f340a20ce72a5
Ryan Franco <rfranco92@gmail.com>
2017-11-10 11:40:43 -0500

    changed name from router to app

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(passport.session());
 const routes = require("./routes");
 app.use(routes);
 
-router.use(function(req, res) {
+app.use(function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
